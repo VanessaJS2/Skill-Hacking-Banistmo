6bbd66c1f1131c3ebfbf8cdbeb33525682c09b37
Vane <vjaramillos@choucairtesting.com>
2025-08-22 10:37:34 -0500

    Cambios validacion PDF

--- src/test/java/co/com/banistmo/prohibition/stepdefinitions/ProhibitionPDFStepDefinitions.java
+++ src/test/java/co/com/banistmo/prohibition/stepdefinitions/ProhibitionPDFStepDefinitions.java
@@ -10,15 +10,17 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-
-import java.util.regex.Matcher;
 
 import static co.com.banistmo.prohibition.constants.Constants.URL;
 import static co.com.banistmo.prohibition.constants.Constants.USER;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.*;
+import static org.hamcrest.Matchers.containsString;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+
 
 public class ProhibitionPDFStepDefinitions {
 
@@ -51,14 +53,17 @@ public class ProhibitionPDFStepDefinitions {
 
     @Then("el PDF de Prohibiciones se muestra correctamente")
     public void elPDFDeProhibicionesSeMuestraCorrectamente() {
+        String texto = "Prohibiciones";
+        var d = BrowseTheWeb.as(theActorInTheSpotlight()).getDriver();
+        var tabs = new java.util.ArrayList<>(d.getWindowHandles());
+        d.switchTo().window(tabs.get(tabs.size()-1));
 
-        theActorInTheSpotlight().should(seeThat(ValidarPdf.texto("Prohibiciones")));
+        theActorInTheSpotlight().should(
+                seeThat( ValidarPdf.leer(), containsString(texto))
 
-
+        );
     }
-
 }
-
 
 
 
